ed27dd59069fffff33240b68ee5af51315254f29
Rodolpho Sousa <rodolphoerick90@gmail.com>
2025-01-20 12:49:23 -0300

    Adding tests for video.go

--- domain/video.go
+++ domain/video.go
@@ -1,12 +1,31 @@
 package domain
 
 import (
+	"github.com/asaskevich/govalidator"
 	"time"
 )
 
 type Video struct {
-	ID         string
-	ResourceID string
-	FilePath   string
-	CreatedAt  time.Time
+	ID         string    `valid:"uuid"`
+	ResourceID string    `valid:"notnull"`
+	FilePath   string    `valid:"notnull"`
+	CreatedAt  time.Time `valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true) //Valida e garante que todos os campos requeridos serão seguidos.
+}
+
+func NewVideo() *Video {
+	return &Video{} //Retorna a referência ao struct Video
+}
+
+func (video *Video) Validate() error {
+	_, err := govalidator.ValidateStruct(video)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
