fc2278b57fc1aa563d9409aa663980f2ff2586f6
Rodolpho Sousa <rodolphoerick90@gmail.com>
2025-02-12 16:55:38 -0300

    Test video repository.

---
+++ application/repositories/video_repository_test.go
@@ -0,0 +1,30 @@
+package repositories_test
+
+import (
+	"domain/application/repositories"
+	"domain/domain"
+	"domain/framework/database"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestNewVideoRepositoryDBInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDB{Db: db}
+	repo.Insert(video)
+
+	v, err := repo.Find(video.ID)
+
+	require.NotEmpty(t, v.ID)
+	require.Nil(t, err)
+	require.Equal(t, video.ID, v.ID)
+}
